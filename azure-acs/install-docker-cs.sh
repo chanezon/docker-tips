1f124ae5b12a0299fff11df56a831ef3cf486f4d
Patrick Chanezon <patlist@chanezon.com>
2016-03-07 12:56:47 -0800

    added -y

--- azure-acs/install-docker-cs.sh
+++ azure-acs/install-docker-cs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 curl -s 'https://sks-keyservers.net/pks/lookup?op=get&search=0xee6d536cf7dc86e2d7d56f59a178ac6c6238f52e' | sudo apt-key add --import
-sudo apt-get update && sudo apt-get install apt-transport-https
+sudo apt-get update && sudo apt-get install -y apt-transport-https
 sudo apt-get install -y linux-image-extra-virtual
 echo "deb https://packages.docker.com/1.10/apt/repo ubuntu-wily main" | sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt-get update && sudo apt-get install docker-engine
+sudo apt-get update && sudo apt-get install -y docker-engine
